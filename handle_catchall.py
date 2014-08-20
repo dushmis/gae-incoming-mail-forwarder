6e89019b14d0263fa6e450087b81dfb2cbdd9684
dushyant <dushmis@gmail.com>
2014-08-20 16:46:48 +0530

    save attachment

--- handle_catchall.py
+++ handle_catchall.py
@@ -7,6 +7,21 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp.mail_handlers import InboundMailHandler
 
+from google.appengine.ext import ndb
+
+from google.appengine.ext import blobstore
+from google.appengine.api import files
+
+
+
+class mmails(ndb.Model):
+  """Models an individual guestbook entry with author, content, and date."""
+  from_ = ndb.StringProperty()
+  subject = ndb.StringProperty()
+  content = ndb.TextProperty()
+  to_ = ndb.TextProperty()
+  date = ndb.DateTimeProperty(auto_now_add=True)
+
 class LogSenderHandler(InboundMailHandler):
   data=""
   subject=""
@@ -21,12 +36,28 @@ class LogSenderHandler(InboundMailHandler):
           subject=self.subject,
           body="",
           html=self.data)
+          
+  def save(self, name, content):
+    file_name = files.blobstore.create(
+      mime_type='application/octet-stream',
+      _blobinfo_uploaded_filename=name
+    )
+    
+    logging.info("saving file "+file_name);
+
+    with files.open(file_name, 'a') as f:
+      f.write(content)
+
+    files.finalize(file_name)
+    return name
+
 
   def receive(self, mail_message):
     self.logme("================================")
     self.logme("Received a mail_message from: " + mail_message.sender)
-    self.subject=mail_message.subject
-    self.logme("The email subject: " + mail_message.subject)
+    if hasattr(mail_message,"subject"):
+      self.subject=mail_message.subject
+      self.logme("The email subject: " + mail_message.subject)
     self.logme("The email was addressed to: " + str.join(str(mail_message.to), ', '))
 
     try:
@@ -51,24 +82,32 @@ class LogSenderHandler(InboundMailHandler):
     for body in plaintext_bodies:
       allBodies = allBodies + " " + body[1].decode()
     self.logme("plaintext_bodies: " + allBodies);
-
+    
     attachments = []
-    try:
-      if mail_message.attachments :
-        if isinstance(mail_message.attachments[0], basestring):
-          attachments = [mail_message.attachments]
-        else:
-        	attachments = mail_message.attachments
-    except exceptions.AttributeError :
-      self.logme("This email has no attachments.")
+    my_file = []
+    my_list = []
+    if hasattr(mail_message, 'attachments'):
+      file_name = ""
+      file_blob = ""
+      for filename, filecontents in mail_message.attachments:
+        file_name = filename
+        file_blob = filecontents.decode()
+        my_file.append(file_name)
+        self.save(file_name,file_blob)
 
     self.logme("number of attachments: " + str(len(attachments)))
+    self.logme("number of my_file: " + str(len(my_file)))
+    self.logme("number of my_list: " + str(len(my_list)))
 
     for filename, content in attachments:
       self.logme("filename: " + filename)
+      
 
     self.logme("--------------------------------")
     self.send_mail_()
+    allBodies=u''.join(allBodies).encode('utf-8').strip()
+    mm = mmails(from_=mail_message.sender,subject=self.subject,content = allBodies,to_=str.join(str(mail_message.to), ', '))
+    mm.put()
 
 
 
